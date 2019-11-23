463a122cb0a7989a76859d4de5768cafaf87046d
bukbuk1231 <loudakkk@hotmail.com>
2019-11-22 18:16:49 -0800

    init commit

---
+++ src/Roster.java
@@ -0,0 +1,18 @@
+public class Roster {
+
+    private String name;
+    private String number;
+
+    public Roster(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getCourseName() {
+        return name;
+    }
+
+    public void addStudent(Student student) {
+
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public void addAssignmentScore(double as) {
+
+    }
+
+    public void addExamScore(double es) {
+
+    }
+}
